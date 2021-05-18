4fc65baf2f35cdb750415ee2fa4961cbe6326f91
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-05-18 17:14:36 -0300

    Meu primeiro códgio CSharp escrito no Bootcamp MRV .NET Developer

---
+++ README.md
@@ -0,0 +1,3 @@
+#**Bootcamp MRV .NET Developer**
+
+###Este repositório foi criado para armazenar os código de estudo do Bootcamp MRV .NET Developer.
---
+++ console01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+            for(int i = 0; i < numeroDeVezes; i++){
+                Console.WriteLine($"{i} - Bem vindo ao Curso de .NET");
+            }
+        }
+    }
+}
