aea47cf44f76f3f41cf039b1523d38948e096b75
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-05-26 14:38:01 -0300

    Inclusão do projeto Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Classes.Heranca
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ponto p1 = new Ponto(10, 20);
+            Ponto3D p2 = new Ponto3D(10, 20, 30);
+        }
+    }
+}
